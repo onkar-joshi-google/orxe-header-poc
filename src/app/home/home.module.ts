4d40e7a634a71d1866b197b5d4eadd51dde03b64
Sachin <sgutte@tavisca.com>
2020-05-05 11:37:18 +0530

    feat: culture core and angular module integration

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,14 +2,15 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
-
+import { CultureModule } from '@orxe-culture/angular';
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    CultureModule
   ],
   schemas:[
     CUSTOM_ELEMENTS_SCHEMA
